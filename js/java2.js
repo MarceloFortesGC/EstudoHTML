634a894e5e05cb06141eb787d2638b20a66baa84
Marcelo Fortes <marcelofortes306@gmail.com>
2020-10-11 17:26:35 -0300

    Atualizações JS

--- js/java2.js
+++ js/java2.js
@@ -1,3 +1,4 @@
+//FUNÇÕES
 console.log("Ola Mundo");
 
 function soma(a, b){
@@ -5,3 +6,15 @@ function soma(a, b){
 }
 
 soma(2,2);
+
+function multiplicacao(a, b){
+    return a*b;
+}
+
+console.log(multiplicacao(5,5));
+
+
+//EVENTO
+function mensagem(){
+    alert("V-você clicou em mim?")
+}
